ec11d15b15aed9e7bde99126a5835b769c6845f1
Alexander Lyons <alexander.n.lyons@gmail.com>
2017-11-01 22:01:20 -0400

    feat: add roster list

--- src/components/rosterWidget/RosterWidgetUI.jsx
+++ src/components/rosterWidget/RosterWidgetUI.jsx
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 
+function compare(a, b) {
+    if (a.totalRaisedAmount < b.totalRaisedAmount)
+        return -1;
+    if (a.totalRaisedAmount > b.totalRaisedAmount)
+        return 1;
+    return 0;
+};
+
 class RosterWidgetUI extends Component {
     render() {
+        const { roster } = this.props;
         let componentToRender;
 
-        componentToRender = (
-            <p>Here is where the team list goes</p>
-        );
+        if (roster.length === 0) {
+            componentToRender = (
+                <p>No members in the roster.</p>
+            );
+        } else {
+            const rosterOrdered = roster.sort(compare).reverse();
+            const rosterItems = rosterOrdered.map((member, i) => (
+                <li>
+                    <p>{member.displayName}</p>
+                    <p>{member.totalRaisedAmount}</p>
+                </li>
+            ));
+            componentToRender = (
+                <ol>{rosterItems}</ol>
+            );
+        }
 
         return (
             <div id='roster'>
