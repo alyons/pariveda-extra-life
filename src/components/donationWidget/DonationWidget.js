3f3b8afd29039a87d43bf1554a43d422c18847f1
Alexander Lyons <alexander.n.lyons@gmail.com>
2017-11-01 22:50:41 -0400

    feat: add functionality to donation widget

--- src/components/donationWidget/DonationWidget.js
+++ src/components/donationWidget/DonationWidget.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import DonationWidgetUI from './DonationWidgetUI';
+import { getName, getFundraisingGoal, getTotalRaisedAmount } from '../../selectors/team';
 
 function mapStateToProps(state, props) {
     return {
-        team: state.team
+        name: getName(state),
+        fundraisingGoal: getFundraisingGoal(state),
+        totalRaisedAmount: getTotalRaisedAmount(state)
     }
 };
 
--- src/components/donationWidget/DonationWidgetUI.jsx
+++ src/components/donationWidget/DonationWidgetUI.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import Progress from 'react-progressbar';
 
 class DonationWidgetUI extends Component {
     render() {
+        const { name, totalRaisedAmount, fundraisingGoal } = this.props;
+        let donationProgress = 0;
         let componentToRender;
 
-        componentToRender = (
-            <p>Verbage about components goes here...</p>
-        );
+        if (fundraisingGoal) {
+            donationProgress = Math.min((totalRaisedAmount / fundraisingGoal * 100), 100);
+            componentToRender = (
+                <div>
+                    <Progress completed={donationProgress} />
+                    {name} have raised ${totalRaisedAmount} out of ${fundraisingGoal}.
+                </div>
+            );
+        } else {
+            componentToRender = (
+                <p>Verbage about components goes here...</p>
+            );
+        }
 
         return (
             <div id='donation'>
